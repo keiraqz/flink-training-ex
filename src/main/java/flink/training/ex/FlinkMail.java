4a33c5bdcbb54e2a97d930597fa2026c57674fe9
keiraqz <keira.qzhou@gmail.com>
2015-12-24 20:16:21 -0500

    update ex1

--- src/main/java/flink/training/ex/FlinkMail.java
+++ src/main/java/flink/training/ex/FlinkMail.java
@@ -29,6 +29,8 @@ import org.apache.flink.api.java.tuple.Tuple3;
  * Following Data Artisan Flink Training:
  * http://dataartisans.github.io/flink-training/exercises/mailData.html
  *
+ * @author Keira Zhou
+ * @date 12/24/2015
  */
 public class FlinkMail {
 
@@ -39,20 +41,20 @@ public class FlinkMail {
 
 		// read sender and body fields
 		DataSet<Tuple2<String, String>> timestampSender = env
-				.readCsvFile(
-						"/Users/Shanghai/Developer/Flink/quickstart/dataset/flinkMails.gz")
-				.lineDelimiter("##//##").fieldDelimiter("#|#")
-				.includeFields("011").types(String.class, String.class);
+				.readCsvFile("dataset/flinkMails.gz").lineDelimiter("##//##")
+				.fieldDelimiter("#|#").includeFields("011")
+				.types(String.class, String.class);
 
 		// get the month and the sender email
 		DataSet<Tuple3<String, String, Integer>> output = timestampSender
-				.map(new MailProcessor()) // map (month, sender) to (month, sender, 1)
+				.map(new MailProcessor()) // map (month, sender) to (month,
+											// sender, 1)
 				.groupBy(0, 1) // group by (month, sender)
 				.reduce(new MailCounter()); // aggregate (month, sender)
 
 		// print the result
 		output.print();
-		
+
 		// data sink
 
 		// execute program
@@ -66,15 +68,16 @@ public class FlinkMail {
 		@Override
 		public Tuple3<String, String, Integer> map(Tuple2<String, String> arg0)
 				throws Exception {
-			
+
 			// get the year and month
 			String[] timeStampLine = arg0.f0.split("-");
 			String timeStamp = timeStampLine[0] + "-" + timeStampLine[1];
-			
+
 			// get the senders' emails
 			String[] senderLine = arg0.f1.split(" ");
-			String sender = senderLine[senderLine.length - 1].replaceAll("[><]", "");
-			
+			String sender = senderLine[senderLine.length - 1].replaceAll(
+					"[><]", "");
+
 			// map (month, sender) to (month, sender, 1)
 			Tuple3<String, String, Integer> output = new Tuple3<String, String, Integer>(
 					timeStamp, sender, 1);
@@ -90,7 +93,7 @@ public class FlinkMail {
 		public Tuple3<String, String, Integer> reduce(
 				Tuple3<String, String, Integer> arg0,
 				Tuple3<String, String, Integer> arg1) throws Exception {
-			
+
 			// aggregate (month, sender)
 			return new Tuple3<String, String, Integer>(arg0.f0, arg0.f1,
 					arg0.f2 + arg1.f2);
